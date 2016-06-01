7204541f7c4fd6b26b18339b5462f4e6472dba39
anshulkthr <anshulkthr34@gmail.com>
2016-06-01 12:41:13 +0530

    change in app credentials

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,8 +3,8 @@ ShopifyApp.configure do |config|
   config.api_key = "3e727b0c906fc08cad719cfc07b27522"
   config.secret = "3f9c4f909da847630999c28fd726de74"
  else
-  config.api_key = "6008c0abacbd0df55b8f1788eadccc2f"
-  config.secret = "59fc59332747d8517ec610985cb19072"
+  config.api_key = "85fcc429823594e34a8013330ad402a8"
+  config.secret = "f061a7c56398fae83e843c42167c43e7"
  end
   config.scope = "read_orders, read_products, write_products, read_content, write_content, read_themes, write_themes"
   config.embedded_app = false
